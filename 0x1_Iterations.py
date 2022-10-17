a0c753cffa54cfef5f7c4c5c8da51ef61250e5ad
LaxrLee <stangathagu95@gmail.com>
2022-10-18 00:23:38 +0300

    deus

--- 0x1_Iterations.py
+++ 0x1_Iterations.py
@@ -16,7 +16,8 @@ for i in range(0, 100):
 for i in range (100):
     print(i)
 
-# Example: We are given some positive integer n. Let's compute the factorial of n and assign it to the variable factorial. The factorial of n is n! = 1 . 2 ... n
+# Example: We are given some positive integer n. Let's compute the factorial of n and assign it to the variable factorial. 
+# The factorial of n is n! = 1 . 2 ... n
 #  We can obtain it by starting with 1 and multiplying it by all the integers from 1 to n
 
 factorial = 1
@@ -24,11 +25,25 @@ for i in range(1, n + 1):
     factorial *=1
 
 
-# Let's print a triangle made of asteriks(*). The trianglke should consist of n rows, where n is a given positive integer, and consecutive rows should contain 1,2,...n
+# Let's print a triangle made of asteriks(*). The trianglke should consist of n rows, where n is a given positive integer, 
+# and consecutive rows should contain 1,2,...n
 # in asteriks. for example , for n = 4 
 # We need to use two loops, one inside the other: the outer loop should print one row in each stepo and the inner loop should print one asterix in each step.
 
 for i in range(1, n+1):
     for j in range(i):
         print ('*'),
+    print 
+
+# Print a triangle made of asteriks seperated y spaces and consisting of n rows again, but this time upside down, and make it symetrical. 
+# Consecutive rows should contain 2n - 1, 2n - 3, ..., 3, 1 asteriks should be indented by 0, 2, 4, ..., 2(n-1) spaces.
+# the triangle should have n rows, where n is some given positive integer. 
+# this time we use three loops: one outer and two inner loops. The outer loop in each step prints one row of the triangle. 
+# The first inner loop is responsible for printing the asteriks.
+
+for i in range(n, 0, -1):
+    for j in range(n - i):
+        print (' '),
+    for j in range(2 * i - 1):
+        print (' * '),
     print

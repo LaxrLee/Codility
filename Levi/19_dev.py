e3a1e530cf95130df3b0bd3b57a35bcd0a6688f4
LaxrLee <stangathagu95@gmail.com>
2022-10-24 10:13:49 +0300

    setup

--- Levi/19_dev.py
+++ Levi/19_dev.py
@@ -3,17 +3,18 @@
 secret_word = "dev"
 guess = ""
 guess_count = 0
-guess_limit = 3
+guess_limt = 3
 out_of_guesses = False
 
+
 while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ")
+    if guess_count < guess_limt:
+        guess = input("Guess a word: ")
         guess_count += 1
     else:
         out_of_guesses = True
 
 if out_of_guesses:
-    print("You ar out of guesses")
+    print("You are out of guesses")
 else:
     print("You guessed correctly")

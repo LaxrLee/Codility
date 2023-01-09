f38dd97a8011e08cfab5037e809989f3713d4bd1
LaxrLee <stangathagu95@gmail.com>
2023-01-09 14:27:49 +0300

    sol

--- LeetCode/Easy/contains_duplicate.py
+++ LeetCode/Easy/contains_duplicate.py
@@ -12,3 +12,26 @@
 # Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
 # Output: True
 
+# firts solution
+# O(n^2) where n is the size of the input array - this is the brute force method
+# good thing is no extra memory is needed
+
+# second soluiton
+# sorting - if we sort then any duplicates that do exist, will be adjacent, meaning just checking the neighbours and then shift pointers to the next spot until the array is finished
+# time complexity is O but sorting does take more time complexity and more memory but no extra space will be used --> O(nlogn)
+
+# if we don't sort our input, we're given the default input but we use extra memory, we use a hashset. 
+# What is a hashset? it will allow us to insert elements into a hashset in big O(1) and also allow us to check. Does a certain value exist, so 1 does not exist hence it is not a duplicate,
+# time complexity is O(n) but does take more space as it will take up the size of the array in the hashset O(n)
+
+# code
+
+class Solution:
+    def constainsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
